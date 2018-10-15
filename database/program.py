58fd9b373a75d1b517eff63553b1a9753d758934
arush15june <ahujaarush@gmail.com>
2018-10-16 00:15:28 +0530

    Build the AbstractProgram class

--- database/program.py
+++ database/program.py
@@ -1,25 +1,37 @@
-import sys
-import inspect
-
 """
     AbstractProgram for building a custom program.
 
     To build a custom program for execution via the REST API:
-        - Create a new .py file in ./database/.
-        - Import 
+        - Open `programs.py` for editing.
+        - Extend the AbstractProgram class for your program. Follow the example program
 """
-
-def programInfo(program_name):
-    if program_name == '':
-        return None
-    pass
 
 class AbstractProgram(object):
     """
         AbstractProgram
         Defaults:
+            Program Name: Class Name
             Inputs: 1 Integer
             Outputs: 1 Integer
+
+        To create a new program:
+            - Extend this class i.e AbstractProgram
+            - Define the `PROGRAM_NAME`
+            - Define the no of inputs `N_INPUTS`
+            - Define the no of outputs `N_OUTPUTS`
+            
+            Example Program:
+
+                class MyProgram(AbstractProgram):
+                    PROGRAM_NAME = 'MyProgram'
+                    N_INPUTS = [int]
+                    N_OUTPUTS = [int]
+                    
+                    def execute(self, *args, **kwargs):
+                        # Returns the square of the passed number
+                        number = args[0]
+                        
+                        return number**2
     """
     
     PROGRAM_NAME = ''
@@ -29,6 +41,17 @@ class AbstractProgram(object):
     def __init__(self, *args, **kwargs):
         self._args = args
         self._kwargs = kwargs
+
+    @property
+    def program_name(self, *args, **kwargs):
+        """
+            Returns PROGRAM_NAME if it is defined 
+            else the name of the class.
+        """
+        if self.PROGRAM_NAME is not '':
+            return self.PROGRAM_NAME
+        else:
+            return self.__class__.__name__
 
     @property
     def inputs(self):        
@@ -47,9 +70,12 @@ class AbstractProgram(object):
             ouputs and no of outputs required for the program.
         """
         return {
-            'n': len(self.N_OUPUTS),
-            'types': self.N_OUPUTS
+            'n': len(self.N_OUTPUTS),
+            'types': self.N_OUTPUTS
         }
 
     def execute(self, *args, **kwargs):
+        """
+            Execute the program.
+        """
         raise NotImplementedError
